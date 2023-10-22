291715b9720bb1cb5424d669d236ad68989158d0
LAURA-DANIELA BOTIȘ <laura.botis@stud.ubbcluj.ro>
2023-10-22 23:56:05 +0300

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using Botis_Laura_Lab2.Migrations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Botis_Laura_Lab2.Models
+{
+    public class Book
+    {
+        public string ID { get; set; }
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        public int? PublishingID { get; set; }
+        public PublishingDate? Publisher {  get; set; } //navigation property
+    }
+}
